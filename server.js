0bd90f02ddbb1c5dd4c642afb61bc0cb43503fa5
Mahendra Mishra <Mahendra@Mahendras-MacBook-Pro.local>
2022-08-29 11:22:56 -0400

    WIP: Add mongoose and db connection

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 // Deoendencies
-const express = require('express')
-
-// configure setting
-require('dotenv').config()
-
-const PORT = process.env.PORT
+const express = require('express');
+const mongoose = require('mongoose');
 
 // Initalize
-const app = express()
+const app = express();
+
+// configure setting
+require('dotenv').config();
+const PORT = process.env.PORT;
+const DATABASE_URI = process.env.DATABASE_URI;
+const db = mongoose.connection;
 
 // Connect to mongodb
+mongoose.connect(DATABASE_URI);
+// add mongoDB connected and error event listners
+db.on('connected', () =>{
+    console.log('connected to MongoDB');
+})
+db.on('error', (err) =>{
+    console.log('MongoDB Error:' + err.message);
+})
+
 
 // mount middleware
 
